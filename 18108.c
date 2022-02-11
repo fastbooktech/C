08d30427572ca51b419de0361e7e2465393fe01c
fastbooktech <99191301+fastbooktech@users.noreply.github.com>
2022-02-11 16:53:25 +0900

    Add files via upload
    18108

---
+++ 18108.c
@@ -0,0 +1,12 @@
+//fastbooktech
+//18108
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int bullgi;
+	scanf("%d", &bullgi);
+	int answer = 2541 - 1998;
+	printf("%d", bullgi-answer);
+	return 0;
+}
