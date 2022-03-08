9f440a9c2b4935b6e3ca84cc2e953515096688db
fastbooktech <99191301+fastbooktech@users.noreply.github.com>
2022-03-08 20:47:33 +0900

    2022.03.08
    #11654

---
+++ 11654.c
@@ -0,0 +1,15 @@
+
+
+//º»
+//2022.03. 08
+//fastbooktech
+//#11654
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	char input;
+	scanf("%c", &input);
+	printf("%d", input);
+	return 0;
+}
